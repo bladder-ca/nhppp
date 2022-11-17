071e437308d11551b36e0e567c79484ba111d002
TA Trikalinos <ttrikalin@mac.com>
2022-11-17 18:56:03 -0500

    changed gitignore to exclude compiled code

--- R/show-speed_comparison.R
+++ R/show-speed_comparison.R
@@ -6,7 +6,7 @@ if(FALSE) {
     sim_nhppp_ct_inv(0, 10, "L", "Linv", FALSE)[1],
     sim_nhppp_ct_thinning(0, 10, 20, "l", TRUE),
     sim_nhppp_ct_thinning(0, 10, 20, "l", FALSE)[1],
-    times = 1000)
+    times = 2000)
 
   # the vignette function
   l_vignette_R <- function(t, r=2) {
