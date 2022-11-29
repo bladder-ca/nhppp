8d6a66be8fa9143911f4a4588e4b7c0d3a3cd3fa
Tom Trikalinos <ttrikalin@mac.com>
2022-11-29 13:58:14 -0500

    added majorization function to the thinning

--- tests/testthat/test-sim_nhppp_thinning.R
+++ tests/testthat/test-sim_nhppp_thinning.R
@@ -2,22 +2,22 @@ test_that("sim_nhppp_thinning() works", {
   l <- function(t) {
     return(2)
   }
-  lmax <- 2
+  lmaj <- 2.2
   S <- methods::new("rstream.mrg32k3a")
 
   expect_no_error(withr::with_preserve_seed(sim_nhppp_t_thinning(lambda = l)))
-  expect_no_error(withr::with_preserve_seed(df <- sim_nhppp_t_thinning(lambda = l, lambda_max = lmax)))
+  expect_no_error(withr::with_preserve_seed(df <- sim_nhppp_t_thinning(lambda = l, lambda_maj = lmaj)))
   expect_true(max(df) <= 10)
 
   # works when range_t[1]>0
-  expect_no_error(withr::with_preserve_seed(sim_nhppp_t_thinning(lambda = l, lambda_max = lmax, range_t = c(5, 10))))
+  expect_no_error(withr::with_preserve_seed(sim_nhppp_t_thinning(lambda = l, lambda_maj = lmaj, range_t = c(5, 10))))
 
   # works with only1 = TRUE
   expect_no_error(
     withr::with_preserve_seed(
       df1 <- sim_nhppp_t_thinning(
         lambda = l,
-        lambda_max = lmax,
+        lambda_maj = lmaj,
         range_t = c(5, 10),
         only1 = TRUE
       )
@@ -27,6 +27,54 @@ test_that("sim_nhppp_thinning() works", {
 
 
   # works with rstream generator
-  expect_no_error(withr::with_preserve_seed(sim_nhppp_t_thinning(lambda = l, lambda_max = lmax, range_t = c(5, 10), rng_stream = S)))
-  rm(list = c("l", "lmax", "S"))
+  expect_no_error(withr::with_preserve_seed(sim_nhppp_t_thinning(lambda = l, lambda_maj = lmaj, range_t = c(5, 10), rng_stream = S)))
+  rm(list = c("l", "lmaj", "S"))
 })
+
+
+test_that("sim_nhppp_thinning() works with linear majorization function", {
+  l <- function(t) {
+    return(2)
+  }
+  S <- methods::new("rstream.mrg32k3a")
+
+  # works with zero slope majorization function
+  expect_no_error(withr::with_preserve_seed(
+    df <- sim_nhppp_t_thinning(
+      lambda = l,
+      lambda_maj = c(2.2, 0),
+      range_t = c(5, 10)))
+  )
+  expect_true(max(df) <= 10)
+
+
+  # works with negative slope majorization function
+  expect_no_error(withr::with_preserve_seed(
+    df <- sim_nhppp_t_thinning(
+      lambda = l,
+      lambda_maj = c(50, -1),
+      range_t = c(5, 10)))
+  )
+  expect_true(max(df) <= 10)
+
+  # works with positive slope majorization function
+  expect_no_error(withr::with_preserve_seed(
+    df <- sim_nhppp_t_thinning(
+      lambda = l,
+      lambda_maj = c(2, .1),
+      range_t = c(5, 10)))
+  )
+  expect_true(max(df) <= 10)
+
+  # fails when majorization function is below l
+  # this is only when you sample at that point
+  expect_error(withr::with_preserve_seed(
+    sim_nhppp_t_thinning(
+      lambda = l,
+      lambda_maj = c(2, -.1),
+      range_t = c(5, 10)))
+  )
+
+
+})
+
