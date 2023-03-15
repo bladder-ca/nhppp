a99bc50bec2a2603fbe7fde7eda0ab65c728fc2d
bladder-ca <sereda_yuliia@brown.edu>
2023-03-15 15:49:34 -0400

    Update utils-microbenchmark.R

--- benchmarks/utils-microbenchmark.R
+++ benchmarks/utils-microbenchmark.R
@@ -47,31 +47,44 @@ ggplot2::autoplot(res, type = "violin")
 
 
 # _t_ vs _n_ functions for non-recurrent events
-inversion_n <- function() {
-  nhppp_n_cumulative_intensity(size = 1, Lambda = L, range_t = rngt)
-}
-inversion_t <- function() {
+hppp_t_loop <-function() {
   t_ <- vector(mode = "numeric")
-  while (length(t_) < 1) {
+  while (length(t_) == 0) {
+    t_ <- ppp_t(rate = 1, range_t = rngt, only1 = TRUE)
+  }
+  return(t_)
+}
+
+nhppp_inversion_t_loop <- function() {
+  t_ <- vector(mode = "numeric")
+  while (length(t_) == 0) {
     t_ <- nhppp_t_cumulative_intensity_inversion(Lambda = L, range_t = rngt, only1 = TRUE)
   }
   return(t_)
 }
-thinning_n <- function() {
-  nhppp_n_intensity(size = 1, lambda = l, lambda_maj = c(0, 2), range_t = rngt)
-}
-thinning_t <- function() {
+
+nhppp_thinning_t_loop <- function() {
   t_ <- vector(mode = "numeric")
-  while (length(t_) < 1) {
+  while (length(t_) == 0) {
     t_ <- nhppp_t_intensity(lambda = l, lambda_maj = c(0, 2), range_t = rngt, only1 = TRUE)
   }
   return(t_)
 }
 
 bench::mark(
-  "inversion_n" = inversion_n(),
-  "inversion_t" = inversion_t(),
-  "thinning_n" = thinning_n(),
-  "thinning_t" = thinning_t(),
+  "ppp_n" = ppp_n(size = 1, range_t = rngt, rng_stream = NULL),
+  "ppp_t&loop" = hppp_t_loop(),
+  "nhppp_inversion_n" = nhppp_n_cumulative_intensity(size = 1, Lambda = L, range_t = rngt),
+  "nhppp_inversion_t&loop" = nhppp_inversion_t_loop(),
+  "nhppp_thinning_n" = nhppp_n_intensity(size = 1, lambda = l, lambda_maj = c(0, 2), range_t = rngt),
+  "nhppp_thinning_t&loop" = nhppp_thinning_t_loop(),
+  check = FALSE, iterations = nsim
+)
+
+# rng_ functions
+bench::mark(
+  "unif" = rng_stream_runif(size = 1, rng_stream = NULL),
+  "exp" = rng_stream_rexp(size = 1, rate = 0.5, rng_stream = NULL),
+  "pois" = rng_stream_rpois(size = 1, lambda = 0.5, rng_stream = NULL),
   check = FALSE, iterations = nsim
 )
