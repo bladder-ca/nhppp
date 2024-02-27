9728a98b0211975574fd34685bf540dd8ab9f224
bladder-ca <sereda_yuliia@brown.edu>
2024-02-27 08:17:54 -0500

    Update example-task.R

--- refactor/example-task.R
+++ refactor/example-task.R
@@ -49,6 +49,8 @@ lesion_times <- vdraw_intensity_step_regular(
   )
 toc()
 
+# Sampling conditional of ≥1 lesion
+
 for(i in 1:5) {
   tic()
   death_other_causes <- nhppp::vztdraw_sc_step_regular_cpp(Lambda_matrix = L_mat,
@@ -64,5 +66,119 @@ for(i in 1:5) {
   )
   toc()
 }
+
+# Transition parameters
+
+edges_dt <- data.table::data.table(
+  edge_start = c("PUNLMP [VLIP]", "Ta-LG [HIP LG]",
+                       "T1-LG [HIP LG]", "T1-LG [HIP LG]",
+                       "Ta-HG [HIP HG]", "T1-HG [HIP HG]",
+                       "T1-HG [HIP HG]", "Tis [CIS]",
+                       "Tis [CIS]", "T1-HG [CIS]",
+                       "T1-HG [CIS]",  "T1-HG [NUC]",
+                       "T1-HG [NUC]", "Locally advanced disease",
+                       "Locally advanced disease", "Metastasis"
+     ),
+  edge_end = c("Ta-LG [HIP LG]", "T1-LG [HIP LG]",
+                    "Locally advanced disease", "BC death",
+                    "T1-HG [HIP HG]", "Locally advanced disease",
+                    "BC death", "T1-HG [CIS]",
+                    "Locally advanced disease", "Locally advanced disease",
+                    "BC death",  "Locally advanced disease",
+                    "BC death", "Metastasis",
+                    "BC death", "BC death"
+                    ),
+  edge_log_N = c(
+    0.02297436, 0.02297436, 0.01430167, 10^-10,
+    0.02297436, 0.05720668, 0.02261079, 0.02261079,
+    0.5720668, 0.5720668, 0.02261079, 0.05720668,
+    0.022610791, 0.479733, 0.3818439, 0.6868871
+  )
+)
+
+node_dt <- data.table::data.table(
+  starting_states = c("Papilloma [VLIP]", "PUNLMP [VLIP]", "Ta-LG [HIP LG]",
+                     "T1-LG [HIP LG]", "Ta-HG [HIP HG]", "T1-HG [HIP HG]",
+                     "Tis [CIS]", "T1-HG [CIS]", "T1-HG [NUC]",
+                     "Locally advanced disease", "Metastasis", "BC death"
+                     ),
+  starting_state_prob = c(0.00, 0.00, 0.675,
+                          0.00, 0.225, 0.00,
+                          0.09, 0.00, 0.01,
+                          0.00, 0.00, 0.00
+                          )
+)
+
+N_0 <- 1e+06
+N_inf <- 1.8e+11
+growth_rate <- 0.6674412
+N_inf_CIS <- 4.57e+08 # (morphology == "flat")
+N_inf_nonCIS <- 1.8e+11
+
+verhulst_equation <- function(lesion_age) {
+  N_0 * exp(growth_rate * lesion_age) /
+    (1 + (N_0 / (N_inf_CIS + (N_inf_nonCIS - N_inf_CIS) * flat_morphology) ) *
+       (exp(growth_rate * lesion_age) - 1))
+}
+
+evaluate_transition_intensity <- function(lesion_age) {
+  N <- verhulst_equation(lesion_age = lesion_age)
+  edge_log_N * log(N)
+}
+
+# Lesion data table
+lesion_dt <- data.table::as.data.table(
+  list(
+    id = 1:nrow(lesion_times),
+    spawn_age = 40,
+    death_other_causes = death_other_causes,
+    lesion_inception = lesion_times[, 1, drop = FALSE],
+    lesion_age = lesion_times[, 1, drop = FALSE] - 40,
+    lesion_stop_age = death_other_causes - lesion_inception,
+    lesion_starting_state = sample(x = node_dt[, starting_states],
+                                   prob = node_dt[, starting_state_prob],
+                                   replace = TRUE,
+                                   size = nrow(lesion_times)
+                                   )
+  )
+)
+lesion_dt[, "flat_morphology"] <- ifelse(lesion_dt[, lesion_starting_state == "Tis [CIS]"], 1, 0)
+lesion_dt_long <- left_join(lesion_dt, edges_dt,
+                        by = c("lesion_starting_state" = "edge_start"),
+                        relationship = "many-to-many")
+
+
+# Majorizer
+n_lambda_intervals <- 20
+transiton_maj_mat <- matrix(NA, ncol = n_lambda_intervals, nrow = nrow(lesion_dt_long))
+for(r in 1:nrow(lesion_dt_long)) {
+  t_steps <- seq(0, lesion_dt_long[r, lesion_stop_age], length.out = n_lambda_intervals + 1)
+  flat_morphology <- lesion_dt_long[r, flat_morphology]
+  edge_log_N <- lesion_dt_long[r, edge_log_N]
+
+  transiton_maj_mat[r,] <- pmax(
+    evaluate_transition_intensity(t_steps[1:n_lambda_intervals]),
+    evaluate_transition_intensity(t_steps[2:(n_lambda_intervals+1)]))
+}
+
+# Check the majorizer
+a <- seq(0,  lesion_dt_long[5, lesion_stop_age], length.out = 20)
+l <- evaluate_transition_intensity(a)
+l_max <- transiton_maj_mat[5, ]
+plot(a, l)
+lines(a, l_max)
+
+# lesion_transition_age <- vdraw_intensity_step_regular(
+#   lambda = evaluate_transition_intensity,
+#   lambda_args = list(flat_morphology = lesion_dt_long[, flat_morphology],
+#                      edge_log_N = lesion_dt_long[, edge_log_N]
+#                      ),
+#   lambda_maj_matrix = transiton_maj_mat,
+#   range_t = as.matrix(lesion_dt[, .(0, lesion_stop_age)])
+# )
+
+
+
+
 
 
