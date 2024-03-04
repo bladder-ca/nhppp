268266735b7fdb62f73fe991d86659ffc0d0d683
TA Trikalinos <ttrikalin@mac.com>
2024-03-04 12:40:39 -0500

    example task

--- refactor/example-task.R
+++ refactor/example-task.R
@@ -4,13 +4,14 @@ library(tictoc)
 dat <- readRDS("refactor/SEER_WM_50K_data.rds")
 lambda_death <- readRDS("refactor/dt_intensities_death_from_other_causes.rds")
 
+
+#### Piecewise constant function for death from other causes
 L <- lambda_death |>
   dplyr::filter(sex == "male" & race == "white" & cohort == 1940 & intensity == "Lambda") |>
   dplyr::filter(age >=40 & age <= 77) |>
   dplyr::select(age, estimate) |>
   tidyr::pivot_wider( names_from = age, values_from = estimate) |>
   as.matrix()
-
 n_people <- 10000
 L_mat <- L[rep(1, n_people),]
 
@@ -22,15 +23,17 @@ death_other_causes <- nhppp::vztdraw_sc_step_regular_cpp(Lambda_matrix = L_mat,
 
 
 
-lesion_intensity_function <- function(age, lambda_args = list(trunc_age = 80, jump_age = 60), ...) {
+####  Lesion intensity function and its arguments
+lesion_intensity_function <- function(age, lambda_args = list(trunc_age = 80, jump_age = 60)) {
    exp(-10 + 0.08 * age * as.numeric(age < lambda_args$trunc_age) +
          0.08 * lambda_args$trunc_age * as.numeric(age >= lambda_args$trunc_age) +
          0.5 * as.numeric(age >= lambda_args$jump_age))
 }
 
-plot(x = c(40:100), y = lesion_intensity_function(age = c(40:100)))
+#plot(x = c(40:100), y = lesion_intensity_function(age = c(40:100)))
 
 
+#### Majorizer for the lesion intensity function
 n_lambda_intervals <- 20
 l_maj_mat <- matrix(NA, ncol = n_lambda_intervals, nrow = n_people)
 for(r in 1:n_people) {
@@ -38,7 +41,6 @@ for(r in 1:n_people) {
   l_maj_mat[r,] <- pmax(lesion_intensity_function(t_steps[1:n_lambda_intervals]),
                       lesion_intensity_function(t_steps[2:(n_lambda_intervals+1)]))
 }
-
 tictoc::tic()
 lesion_times <- vdraw_intensity_step_regular_cpp(
     lambda = lesion_intensity_function,
@@ -48,7 +50,7 @@ lesion_times <- vdraw_intensity_step_regular_cpp(
   )
 tictoc::toc()
 
-# Sampling conditional of ≥1 lesion
+#### Sampling conditional of ≥1 lesion
 
 for(i in 1:5) {
   tictoc::tic()
@@ -56,7 +58,7 @@ for(i in 1:5) {
                                                            range_t = c(40, 110),
                                                            atmost1 = TRUE)
 
-  lesion_times <- vztdraw_intensity_step_regular(
+  lesion_times <- vztdraw_intensity_step_regular_R(
     lambda = lesion_intensity_function,
     lambda_args = list(trunc_age = 80, jump_age = 60),
     lambda_maj_matrix = l_maj_mat,
@@ -65,8 +67,12 @@ for(i in 1:5) {
   tictoc::toc()
 }
 
-# Transition parameters
 
+
+
+#################################################################
+# Transition parameters
+#################################################################
 
 edges_dt <- data.table::data.table(
   edge_start = c("PUNLMP [VLIP]", "Ta-LG [HIP LG]",
@@ -108,24 +114,17 @@ node_dt <- data.table::data.table(
                           )
 )
 
-N_0 <- 1e+06
-N_inf <- 1.8e+11
-growth_rate <- 0.6674412
-N_inf_CIS <- 4.57e+08 # (morphology == "flat")
-N_inf_nonCIS <- 1.8e+11
 
-verhulst_equation <- function(lesion_age) {
+
+verhulst_equation <- function(lesion_age, flat_morphology, N_0 = 1e+06, N_inf = 1.8e+11, growth_rate = 0.6674412, N_inf_CIS = 4.57e+08, N_inf_nonCIS = 1.8e+11) {
   N_0 * exp(growth_rate * lesion_age) /
     (1 + (N_0 / (N_inf_CIS + (N_inf_nonCIS - N_inf_CIS) * flat_morphology) ) *
        (exp(growth_rate * lesion_age) - 1))
 }
 
-evaluate_transition_intensity <- function(lesion_age, lambda_args=NULL) {
-  if(!is.null(lambda_args)) {
-    list2env(lambda_args)
-  }
-  N <- verhulst_equation(lesion_age = lesion_age)
-  edge_log_N * log(N)
+evaluate_transition_intensity <- function(lesion_age, lambda_args=list(flat_morphology = FALSE, edge_log_N = 0)) {
+  N <- verhulst_equation(lesion_age = lesion_age, flat_morphology = lambda_args$flat_morphology)
+  lambda_args$edge_log_N * log(N)
 }
 
 # Lesion data table
@@ -169,7 +168,7 @@ data.table::setkey(lesion_dt_long, "id")
 tictoc::tic()
 lambda_args <- list(flat_morphology = lesion_dt_long[, flat_morphology],
                     edge_log_N = lesion_dt_long[, edge_log_N])
-n_lambda_intervals <- 20
+n_lambda_intervals <- 5
 range_t = as.matrix(lesion_dt_long[, .(0, lesion_stop_age)])
 n_rows <- nrow(lesion_dt_long)
 t_steps <- matrix(
