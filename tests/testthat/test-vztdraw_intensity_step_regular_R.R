2ac50e1da267a25fed3481eb5c7b39ef5105ed0c
TA Trikalinos <ttrikalin@mac.com>
2024-04-07 15:42:17 -0400

    error in an agreement file

--- tests/testthat/test-vztdraw_intensity_step_regular_R.R
+++ tests/testthat/test-vztdraw_intensity_step_regular_R.R
@@ -2,6 +2,7 @@
 ## add tests that have lamda_args
 
 test_that("vztdraw_intensity_step_regular_R() works", {
+  set.seed(123)
   lfun <- function(x, lambda_args, ...) .2 * x^lambda_args$exponent
   l_args <- list(exponent = 1L)
   lmaj <- matrix(rep(1, 50), ncol = 5)
@@ -51,7 +52,8 @@ test_that("vztdraw_intensity_step_regular_R() works", {
 
 
 test_that("vztdraw_intensity_step_regular_R() does not break with matrices whose mode is list", {
-  lfun <- function(x, ...) .2 * x
+  set.seed(123)
+  lfun <- function(x, ...) .2 * x   
   lmaj <- matrix(rep(1, 50), ncol = 5)
   Lmaj <- mat_cumsum_columns(lmaj)
 
@@ -75,31 +77,3 @@ test_that("vztdraw_intensity_step_regular_R() does not break with matrices whose
 })
 
 
-
-test_that("vdraw_intensity_step_regular_R() works with different majorizers", {
-  lfun <- function(x, lambda_args, ...) .2 * x^lambda_args$exponent
-  l_args <- list(exponent = 1L)
-  lmaj <- matrix(rep(1, 1000), ncol = 5)
-
-  expect_no_error(Z1 <- vdraw_intensity_step_regular_R(
-    lambda = lfun,
-    lambda_args = l_args,
-    lambda_maj_matrix = lmaj,
-    range_t = c(1, 5),
-    tol = 10^-6,
-    atmost1 = FALSE
-  ))
-  check_ppp_sample_validity(Z1, t_min = 1, t_max = 5)
-
-  expect_no_error(Z2 <- vdraw_intensity_step_regular_R(
-    lambda = lfun,
-    lambda_args = l_args,
-    lambda_maj_matrix = lmaj + 10,
-    range_t = c(1, 5),
-    tol = 10^-6,
-    atmost1 = FALSE
-  ))
-  check_ppp_sample_validity(Z2, t_min = 1, t_max = 5)
-
-  compare_ppp_vectors(ppp1 = Z1, ppp2 = Z2, threshold = 0.1, showQQ = TRUE)
-})
