4871d754ab6f1f661ad913e9ce205ecdd4cb7a46
TA Trikalinos <ttrikalin@mac.com>
2024-04-11 16:11:29 -0400

    styler

--- tests/testthat/test-ztppp.R
+++ tests/testthat/test-ztppp.R
@@ -10,6 +10,4 @@ test_that("ztppp() arguments work", {
   S1 <- methods::new("rstream.mrg32k3a")
   expect_no_error(df2 <- ppp_sequential(range_t = c(0, 10), rate = 1, rng_stream = S1))
   check_ppp_sample_validity(times = df2, t_min = 0, t_max = 10, atleast1 = TRUE)
-
-
 })
